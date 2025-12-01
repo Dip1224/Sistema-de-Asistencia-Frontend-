c5c64bbf3d9056e780742dd096fe10fb8b30c4e3
dip1224 <diegoarcani190@gmail.com>
2025-12-01 12:28:10 -0400

    frontend 8/8 facejsx

--- frontend/src/components/FaceRecognition.jsx
+++ frontend/src/components/FaceRecognition.jsx
@@ -129,7 +129,7 @@ function FaceRecognition() {
     const canvas = document.createElement("canvas");
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d", { willReadFrequently: true });
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     return canvas;
   }
